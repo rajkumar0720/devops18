959d05e4067dcc811eaa2ff1cd656659cc35dbb7
rajkumar0720 <147323263+rajkumar0720@users.noreply.github.com>
2024-02-22 09:25:47 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -22,3 +22,10 @@ versioning_configuration {
 status = "Enabled"
 }
 }
+terraform {
+  backend "s3" {
+    bucket = "sm7444.flm.bucket"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
